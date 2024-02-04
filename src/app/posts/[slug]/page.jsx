bb0d7155617c4b2c2d519b2b8bbe2093ad77a705
Harsh Shukla <108127262+harshshukla2807@users.noreply.github.com>
2024-02-05 00:25:21 +0530

    deployed links added

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -6,7 +6,7 @@ import Comments from "@/components/Comments/Comments";
 import Loading from "@/app/Loading";
 
 const getData = async (slug) => {
-  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+  const res = await fetch(`https://word-wave-blog-application.vercel.app/api/posts/${slug}`, {
     cache: "no-store",
   });
 
--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -23,7 +23,7 @@ const Comments = ({ postSlug }) => {
   const { status } = useSession();
 
   const { data,mutate, isLoading } = useSWR(
-    `http://localhost:3000/api/comments?postSlug=${postSlug}`,
+    `https://word-wave-blog-application.vercel.app/api/comments?postSlug=${postSlug}`,
     fetcher
   );
 
